c9b971b80b486ed59c7046972c75d039c806c2c6
Naruto <1562007048@qq.com>
2018-06-01 21:53:31 +0800

    xlrd 获取excel数据

--- util/operation_excel.py
+++ util/operation_excel.py
@@ -1,6 +1,29 @@
 import xlrd
 
-data = xlrd.open_workbook('./dataconfig/interface.xlsx')
-tables = data.sheets()[0]
-print(tables.nrows)
-print(tables.cell_value(2,2))
+class OperationExcel:
+    def __init__(self,file_name,sheet_id):
+        self.file_name = file_name
+        self.sheet_id = sheet_id
+        self.data = self.get_data()
+
+    #获取sheets内容
+    def get_data(self):
+        data = xlrd.open_workbook(self.file_name)
+        tables = data.sheets()[self.sheet_id]
+        return tables
+
+    #获取tables行数
+    def get_lines(self):
+        tables = self.get_data()
+        return tables.nrows
+
+    #获取指定单元格内容
+    def get_cell_value(self,row,col):
+        return self.get_data().cell_value(row,col)
+
+if __name__ == '__main__':
+    opers = OperationExcel('./dataconfig/interface.xlsx',0)
+    print(opers.get_data().nrows)
+    print(opers.get_lines())
+    print(opers.get_cell_value(2,2))
+   
