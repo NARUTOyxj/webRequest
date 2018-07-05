9297409ff6bf60365f88465ce01f501cee5ab18f
Naruto <1562007048@qq.com>
2018-07-05 22:05:31 +0800

    数据依赖

--- data/get_data.py
+++ data/get_data.py
@@ -71,6 +71,37 @@ class GetData():
         col = int(data_config.get_result())
         self.opera_excel.write_result(row,col,value)
 
+
+    #获取依赖数据的key
+    def get_depend_key(self,row):
+        col = int(data_config.get_data_depend())
+        depent_key = self.opera_excel.get_cell_value(row,col)
+        if depent_key == '':
+            return None
+        else:
+            return depent_key
+
+    #判断是否有case依赖
+    def is_depend(self,row):
+        col = int(data_config.get_case_depend())
+        depend_case_id = self.opera_excel.get_cell_value(row,col)
+        if depend_case_id == '':
+            return None
+        else:
+            return depend_case_id
+
+    #获取数据依赖字段
+    def get_depend_field(self,row):
+        col = int(data_config.get_filed_depend())
+        data = self.opera_excel.get_cell_value(row,col)
+        if depend_case_id == '':
+            return None
+        else:
+            return data
+
+   
+        
+
         
 
 
--- main/run_test.py
+++ main/run_test.py
@@ -3,6 +3,7 @@ sys.path.append('D:/project/webRequest')
 from base.runmethod import RunMethod
 from data.get_data import GetData
 from util.common_util import CommonUtil
+from data.dependent_data import DependentData
 
 class RunTest:
     def __init__(self):
@@ -15,16 +16,26 @@ class RunTest:
         res = None
         rows_count = self.data.get_case_lines()
         for i in range(1,rows_count):
-            url = self.data.get_url(i)
-            method = self.data.get_request_method(i)
             is_run = self.data.get_is_run(i)
-            data = self.data.get_data_for_json(i)
-            expect = self.data.get_expect_data(i)
-            header = self.data.is_header(i)
             if is_run:
-                res = self.run_method.run_main(method,url,data,header)
+                url = self.data.get_url(i)
+                method = self.data.get_request_method(i)
+                request_data = self.data.get_data_for_json(i)
+                expect = self.data.get_expect_data(i)
+                header = self.data.is_header(i)
+                depend_case = self.data.is_depend(i)
+                if depend_case != None:
+                    self.depend_data = DependentData(i)
+                    #获取依赖的响应数据
+                    depend_reponse_data = self.depend_data.get_data_for_key(i)
+                    #获取依赖的key
+                    depent_key = self.data.get_depend_field(i)
+                    request_data[depend_key] = depend_reponse_data
+                res = self.run_method.run_main(method,url,request_data,header) 
+
                 if self.com_util.is_contain(expect,res):
                     self.data.write_result(i,'pass')
+                    print(res)
                 else:
                     self.data.write_result(i,'fail')
 
