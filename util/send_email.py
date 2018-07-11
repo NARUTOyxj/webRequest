33a09a0a62c1928492db57a99126ab639929aaff
yangxianjie <xianjie_yang@loongjoy.com>
2018-07-11 11:18:48 +0800

    snd_email fix

--- util/send_email.py
+++ util/send_email.py
@@ -7,15 +7,15 @@ class SendEmail:
     global password 
     send_user = 'xianjie_yang@loongjoy.com'
     email_host = 'smtp.exmail.qq.com'
-    password = 'Yxj123456'
+    password = 'Yxj931227'
     def send_mail(self,user_list,sub,content):
         user = 'xianjie_yang@loongjoy.com' + '<' + send_user + '>'
         message = MIMEText(content,_subtype='plain',_charset='utf-8')
         message['Subject'] = sub
         message['From'] = user
         message['To'] = ','.join(user_list)
-        server = smtplib.SMTP()
-        server.connect(email_host,port=465)
+        server = smtplib.SMTP_SSL()
+        server.connect(email_host,465)
         server.login(send_user,password)
         server.sendmail(user,user_list,message.as_string())
         server.close()
